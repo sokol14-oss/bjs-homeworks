411afd0b8dbfb733a2e23bfa36d91900678b122f
Anastasiya Sokolova <nastya.sokol2013@yandex.ru>
2021-04-26 16:31:48 +0500

    1.1-variables

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,28 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
+    let algebra = 5;
+    let geography = 3;
+    let physics = 2;
+    let average = (algebra + geography + physics) / 3;
     
-	return // ваш ответ;
+	return average;
 }
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+    let myName = "Вася"
+    let message = "Привет, мир! Меня зовут" + " " + myName;
+	return message;
 }
 
 function calculateFormula() {
     
-	// реализуйте здесь задание №3
+	let x = 2;
+	let y = 22;
+	 let z = 0;
+	 let result = x * y + 5 * z + x - 1;
+
     
-    return // ваш ответ;
+    return result;
 }
--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,69 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    "use strict";
+    let d = b**2 - 4 * a * c;
+    let res = [];
+    let tmp;
+    if (d < 0)
+    return res;
+    if (d === 0)
+    res = (-b + Math.sqrt(d)) / (2 * a);
+    return res;
+    if (d > 0)
+    let tmp = [];
+        tmp.push((-b + Math.sqrt(d)) / (2 * a));
+        tmp.push((-b - Math.sqrt(d)) / (2 * a));
+        res["quadratic roots"] = tmp;
+    
+    return res;
+
 }
+    
+getResult(2, 4, 3);
+    
+
+   
+
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+let marks = [];
+    let sum = 0;
+    let count = 0;
+    
+    marks.push(2);
+    marks.push(5);
+    marks.push(4);
+    
+    if (marks = [])
+    return 0;
+    if (marks.length > 5) {
+    console.log("Оценок больше 5");
+    marks = marks.slice(0, 5);
+    for (i = 0; i < marks.length; i++)
+    count += 1;
+    sum += numbers[i];
+    res = sum / count;
+    }
+    return res;
+    if (marks.length < 3)
+    console.log("Оценок должно быть не менее 3-х!")
+    else
+    for (i = 0; i < marks.length; i++){
+    count += 1;
+    sum += numbers[i];
+    res = sum / count;
+    }
+    return res;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    llet name = "Vasya";
+  let now = new Date();
+  let dateOfBirthday = new Date(1990, 9, 31);
+ let age = today.getFullYear() - dateOfBirthday.getFullYear();
+ if (age > 18)
+ res = "Не желаете ли олд-фэшн," + " " + name + "?";
+ return res;
+ else
+res = "Сожалею," + " " + name + "," + "но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!";
+return res;
 }
