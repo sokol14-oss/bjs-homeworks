a87a3cec6e847c5d71f275d4137839c72896f1bc
Anastasiya Sokolova <nastya.sokol2013@yandex.ru>
2021-05-15 16:17:04 +0500

    added file

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,34 @@
+"use string";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+  if (percent === ToString(percent) || contribution === ToString(contribution) || amount === ToString(amount) || date === ToString(date)) {
+Number(percent, contribution, amount, date);
+  }
+  else {
+  	let arr = [percent, contribution, amount, date];
+  	for (let i = 0; i < arr.length; i++ ) {
+  	arr.includes(percent, contribution, amount, date);
+  }
+  arr[i].ToString(Параметр $(arr[i]) содержит неправильное значение $(arr[i]).)
+  console.log(arr[i].ToString());
+  }
+  let body = amount - contribution;
+  Data = new Date();
+  let payMonth = date.getMonth() - Data.getMonth() 
+    + (12 * (date.getFullYear() - Data.getFullYear()));
+    let p = (percent / 100) / 12;
+   let pay = body * (p + p / (((1 + P)Math.pow(1 + p, payMonth)) - 1));
+   let totalAmountn.toFixed(2) = pay * payMonth;
+   console.log(Number.totalAmountn);
+
+    return Number.totalAmountn;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (name === true) {
+    	console.log(`Привет, мир! Меня зовут ${name}.`);
+    }
+    else {
+    	console.log(`Привет, мир! Меня зовут ${"Аноним"}.`)
+    }
+    return getGreeting;
 }
