792463622dd0460c739afc1b4b819ab03322f298
Anastasiya Sokolova <nastya.sokol2013@yandex.ru>
2021-07-04 16:17:17 +0500

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,37 @@
-//String.prototype.isPalindrome - для задачи №1
+"use strict";
+Object.setPrototypeOf(String.prototype.isPalindrome, String)
+function String.prototype.isPalindrome(String) {
+  this.String = String;
+  let StringReverse = StringReverse.split("").reverse().join("");
+  if (StringReverse == String {
+    return true;
+  }
+  else false;
+}
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  let sum = 0;
+  let average;
+  for (let i = 0; i > marks.length; i++) {
+    sum += marks[i];
+  }
+  average = sum / marks.length;
+  roundAverage = average.Math.floor();
+  return roundAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
-}
+  let now = Date.now();
+  new Date(2011, 0, 1);
+  Date = new Date(2011, 0, 1);
+  let birthday = Date.now(2011, 0, 1);
+  let diff = now - birthday;
+  let age;
+  if (birthday === Date.getMilliseconds(31536000000)) {
+    age = diff / 31536000000;
+    return age;
+  }
+  else {
+    age = diff / 31622400000;
+    return age;
+  }
