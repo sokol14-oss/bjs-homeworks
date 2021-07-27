7852c62087e5eba5269f5e23a5e16b9d4eb1b2b7
Anastasiya Sokolova <nastya.sokol2013@yandex.ru>
2021-07-27 16:14:30 +0500

    added file

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,12 +1,12 @@
 "use string";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  if ((isNaN(percent)) && percent > 0) {
+  if ((isNaN(percent)) || percent > 0) {
 parseInt(percent);
   }
-  else if ((isNaN(contribution)) && contribution > 0) {
+  else if ((isNaN(contribution)) || contribution > 0) {
 parseInt(contribution);
   }
-   else if ((isNaN(amount)) && amount > 0) {
+   else if ((isNaN(amount)) || amount > 0) {
 parseInt(amount);
   }
   else if (date < new Date()) {
@@ -14,22 +14,33 @@ return 0;
   }
   else {
     let arr = [percent, contribution, amount, date];
-    arr.includes(percent, contribution, amount, date);
-  return `Параметр $(arr) содержит неправильное значение $(arr).`;
+    if (arr.includes(percent)) {
+    	return `Параметр $(percent) содержит неправильное значение $(percent).`;
+    }
+    if (arr.includes(contribution)) {
+    	return `Параметр $(contribution) содержит неправильное значение $(contribution).`;
+    }
+    if (arr.includes(amount)) {
+    	return `Параметр $(amount) содержит неправильное значение $(amount).`;
+    }
+    if (arr.includes(date)) {
+    	return `Параметр $(date) содержит неправильное значение $(date).`;
+    }
   }
   let body = amount - contribution;
    let day = new Date();
   let payMonth = date.getMonth() - day.getMonth() 
     + (12 * (date.getFullYear() - day.getFullYear()));
   let p = (percent / 100) / 12;
-  let pay = body * (p + p / (((1 + p)Math.pow(1 + p, payMonth)) - 1));
+  let pay = body * (p + p / (((1 + p) ** payMonth) - 1));
   let totalAmountn = pay * payMonth;
-  let totalAmountnFixed = parseInt(totalAmountn).toFixed(2);
-  console.log(totalAmountnFixed);
+  let totalAmountnFixed = totalAmountn.toFixed(2);
+  console.log(parseFloat(totalAmountnFixed));
 
-    return totalAmountnFixed;
+    return parseFloat(totalAmountnFixed);
 }
 
 function getGreeting(name) {
- return `Привет, мир! Меня зовут ${name || "Аноним"}`
+ return `Привет, мир! Меня зовут ${name || "Аноним"}.`
 }
+
