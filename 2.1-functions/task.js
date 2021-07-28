dba0cb7a2021dc20bc39278606d057046db19b80
Anastasiya Sokolova <nastya.sokol2013@yandex.ru>
2021-07-28 13:01:15 +0500

    added file

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,4 +1,4 @@
-function  getSolutions( a, b, c ) {
+function  getSolutions(a, b, c) {
 	let D = b** - 4 * a * c;
 	if (D < 0) {
 		return { 
@@ -14,8 +14,8 @@ function  getSolutions( a, b, c ) {
 		};
 	}
 	if (D > 0) {
-		let x1 = (-b + √D) / 2 * a;
-		let x2 = (-b - √D) / 2 * a;
+		let x1 = (-b + Math.sqrt(D) / (2 * a);
+		let x2 = (-b - Math.sqrt(D) / (2 * a);
 		{ 
 			D: D, 
 			roots: [x1, x2] 
@@ -24,19 +24,16 @@ function  getSolutions( a, b, c ) {
 }
 
 function showSolutionsMessage(a, b, c) {
-	let result = { 
-			D: D, 
-			roots: [x1, x2] 
-		};
+	let result = getSolutions({D: D, roots: [x1, x2]});
 	console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
 	console.log(`Значение дискриминанта: ${result.D}`);
- 	if (D < 0) {
+ 	if (result.D < 0) {
  		console.log(`Уравнение не имеет вещественных корней`);
  	}
- 	if (D === 0) {
- 		console.log(`Уравнение имеет один корень ${x₁} = значение_корня`);
+ 	if (result.D === 0) {
+ 		console.log(`Уравнение имеет один корень ${x1} = значение_корня`);
  	}
- 	if (D > 0) {
+ 	if (result.D > 0) {
  		console.log(`Уравнение имеет два корня. ${x1} = значение_корня_1, ${x2} = значение_корня_2`);
  	}
 
@@ -44,19 +41,24 @@ function showSolutionsMessage(a, b, c) {
 
 function getAverageScore(data) {
 data = {};
+for (i = 0; i < data.length; i++) {
+	data[i] = getAverageMark(data, marks);
+}
+data.average = Object.values(data);
+return data;
 }
 
 function getAverageMark(marks) {
 	let sum = 0;
 	let res;
 	
-data.average = for (i = 0; i < marks.length; i++) {
-(sum + marks[i]) / marks.length;
+for (i = 0; i < marks.length; i++) {
+res = (sum + marks[i]) / marks.length;
+return res;
 }
 if (marks.length === 0) {
-	data.average = 0;
+	return = 0;
 }
-console.log(data, data.average);
 }
 
 
